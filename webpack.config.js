368c0608c709d1e9dcb62acda6edb4d9afcc531e
Ko3uHaK <den2001888@yandex.by>
2021-11-17 16:14:05 +0300

    dist html

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,6 @@ module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/template.html'), // шаблон
-            filename: 'index.html', // название выходного файла
-        }),
-    ],
 };
 //Точка выхода
 //точка выхода как «dist». Префикс "[name]" соответствует названию файла в src
@@ -23,4 +16,15 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
     },
+}
+module.exports = {
+    // ...
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
 }
