e5532509e0fb0609e2cf1f2a31ef01d8e86bdd3c
Ko3uHaK <den2001888@yandex.by>
2021-11-17 15:38:46 +0300

    exit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 
 //точка входа
-const path = require('path')
+const path = require('path');
 
 module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
+    },
+};
+//Точка выхода
+//точка выхода как «dist». Префикс "[name]" соответствует названию файла в src
+module.exports = {
+    // ...
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
     },
 }
