75e565555bf74e6769629162b6e0f29cd173d251
Ko3uHaK <den2001888@yandex.by>
2021-11-17 16:10:33 +0300

    plugins for html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-
 //точка входа
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
 };
 //Точка выхода
 //точка выхода как «dist». Префикс "[name]" соответствует названию файла в src
